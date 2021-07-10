8ef0f21a7344036a603d03584ad07b059e931841
preethikishore <preethikishre@gmail.com>
2021-07-10 22:08:28 +0530

    update without error

--- index.js
+++ index.js
@@ -10,26 +10,9 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 let access_token;
-//pan variables
-let panNumber = 1234567;
-let fullName = "Sample";
-let category = "";
-let message = "None";
-//bank Variables
-let bankStatus = false;
-let accHolderName = "Sample";
-let bankMessage = "None";
-let amount = 0;
-let referenceID = 1234;
-var request = require("request");
-//aadhar variables
-let uidStatus = "none";
-let aadharNum = 1234567;
-let aadharMessage = "none";
+let panStatus;
 
 const urlpan = "https://sandbox.aadhaarkyc.io/api/v1/pan/pan";
-const urluid =
-  "https://sandbox.aadhaarkyc.io/api/v1/aadhaar-validation/aadhaar-validation";
 var surepass_post = {
   method: "POST",
   headers: {
@@ -39,62 +22,25 @@ var surepass_post = {
     Accept: "application/json",
   },
 };
-var sandbox_auth = {
-  method: "POST",
-  hostname: "api.sandbox.co.in",
-  path: "/authenticate",
-  headers: {
-    "x-api-key": "key_live_kcZBc6aAIISGjZY6jgbXuNvkkkzywfnZ",
-    "x-api-secret": "secret_live_4AzvZkZI3yvlKZfgF09eI90eR6bTpKrk",
-    "x-api-version": "3.4.0",
-  },
-  maxRedirects: 20,
-};
-
-var urlbank = "https://api.sandbox.co.in";
-var sandbox_get = {
-  method: "GET",
-  hostname: "api.sandbox.co.in",
-  headers: {
-    Authorization:
-      "eyJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJBUEkiLCJyZWZyZXNoX3Rva2VuIjoiZXlKaGJHY2lPaUpJVXpVeE1pSjkuZXlKaGRXUWlPaUpCVUVraUxDSnpkV0lpT2lKMmFXcHBkR2hBYzNKMmFXNW1iM1JsWTJndWFXNGlMQ0poY0dsZmEyVjVJam9pYTJWNVgyeHBkbVZmYTJOYVFtTTJZVUZKU1ZOSGFscFpObXBuWWxoMVRuWnJhMnQ2ZVhkbWJsb2lMQ0pwYzNNaU9pSmhjR2t1YzJGdVpHSnZlQzVqYnk1cGJpSXNJbVY0Y0NJNk1UWTFOek13TlRnME1Dd2lhVzUwWlc1MElqb2lVa1ZHVWtWVFNGOVVUMHRGVGlJc0ltbGhkQ0k2TVRZeU5UYzJPVGcwTUgwLlR1R2F3VDdNMTF1LW4xODJORF83ZGxua2c1WUxONnNsTFppRUs5TVNvdHVIX0d1SmRMZnZTMTZsQnpaUWM2UzhtZUVlUVQydUk1Ynhibl9vb3BGbVVBIiwic3ViIjoidmlqaXRoQHNydmluZm90ZWNoLmluIiwiYXBpX2tleSI6ImtleV9saXZlX2tjWkJjNmFBSUlTR2paWTZqZ2JYdU52a2trenl3Zm5aIiwiaXNzIjoiYXBpLnNhbmRib3guY28uaW4iLCJleHAiOjE2MjU4NTYyNDAsImludGVudCI6IkFDQ0VTU19UT0tFTiIsImlhdCI6MTYyNTc2OTg0MH0.2YMNIvlxLHRHj70SqbRM_xuJpLx_ccuDGcP6fWxbIuzKH8A8UUP26RYalTpOASl8-yW9RJ1hVRz1qazwl1cm-A",
-    "x-api-key": "key_live_kcZBc6aAIISGjZY6jgbXuNvkkkzywfnZ",
-    "x-api-version": "3.4.0",
-  },
-  maxRedirects: 20,
-};
-
-capitalize = function (input) {
-  let word = input.split(" ");
-  capitalize = [];
-  word.forEach((elem) => {
-    capitalize.push(elem[0].toUpperCase() + elem.slice(1, elem.length));
-  });
-  return capitalize.join(" ");
-};
 
 app.get("/", function (req, res) {
-  panNumber = "";
-  fullName = "";
-  category = " ";
-  message = "";
   res.render("pan", {
-    panNum: panNumber,
-    personName: fullName,
-    message: message,
+    panNum: "",
+    personName: "",
+    message: "",
+    panStatus: "",
   });
+  console.log("main");
 });
 
 app.get("/pan", function (req, res) {
-  panNumber = " ";
-  fullName = "";
-  category = "";
-  message = "None";
   res.render("pan", {
-    panNum: panNumber,
-    personName: fullName,
-    message: message,
+    panNum: "",
+    personName: "",
+    message: "",
+    panStatus: "",
   });
+  console.log("pan");
 });
 
 app.post("/pan", function (req, res) {
@@ -108,108 +54,27 @@ app.post("/pan", function (req, res) {
     .then((res) => res.json())
     .then((d) => {
       try {
-        panNumber = d.data.pan_number;
-        fullName = d.data.full_name;
-        category = d.data.category;
-        message = d.message_code;
+        panStatus = d.success;
+        let statusMsg;
+        if (panStatus) {
+          statusMsg = "Verified Successfully";
+        } else {
+          statusMsg = "Failed, Check PAN #";
+        }
         res.render("pan", {
-          panNum: panNumber,
-          personName: fullName,
-          message: message,
+          panNum: d.data.pan_number,
+          personName: d.data.full_name,
+          message: statusMsg,
+          panStatus: panStatus,
         });
+        console.log("Post");
       } catch (e) {
-        res.redirect("/");
+        res.redirect("/pan");
         console.log("not valid data");
         alert.message("Please Enter Valid PAN");
       }
     });
 });
-
-app.get("/aadhar", function (req, res) {
-  aadharMessage = "none";
-  aadharNum = " ";
-  res.render("aadhar", {
-    aadharNum: aadharNum,
-    uidStatus: aadharMessage,
-  });
-});
-
-app.post("/aadhar", function (req, res) {
-  uidnum = req.body.aadhar;
-  surepass_post["body"] = JSON.stringify({
-    id_number: uidnum,
-  });
-  fetch(urluid, surepass_post)
-    .then((res) => res.json())
-    .then((d) => {
-      try {
-        console.log(d);
-        aadharNum = d.data.aadhaar_number;
-        uidStatus = d.message_code;
-        uidSuccess = d.success;
-
-        if (uidSuccess) {
-          aadharMessage = "Aadhar is Verfied Successfully";
-        } else {
-          aadharMessage = "Aadhar Verfication Failed";
-        }
-
-        res.render("aadhar", {
-          aadharNum: aadharNum,
-          uidStatus: aadharMessage,
-        });
-      } catch (e) {
-        res.redirect("/");
-        alert("Please Provide Valid Data");
-        console.log("not valid data");
-      }
-    });
-});
-
-// app.get("/bank", function (req, res) {
-//   fetch(urlbank + "/authenticate", sandbox_auth)
-//     .then((res) => res.json())
-//     .then((d) => {
-//       try {
-//         access_token = d.access_token;
-//         console.log(d.access_token);
-//       } catch (e) {
-//         res.redirect("/");
-//         console.log("not valid data");
-//       }
-//     });
-//   res.render("bank");
-//   var account = req.query.account;
-//   var name = req.query.name.split(" ").join("%20");
-//   var ifsc = req.query.ifsc;
-//   var mobile = req.query.mobile;
-//   var path = `/bank/${ifsc}/accounts/${account}/verify?name=${name}&mobile=${mobile}`;
-//   urlbank = urlbank + path;
-//   sandbox_get["path"] = path;
-//   sandbox_get["headers"]["Authorization"] = access_token;
-//   console.log(sandbox_get);
-//   console.log(urlbank);
-
-//   fetch(urlbank, sandbox_get)
-//     .then((res) => res.json())
-//     .then((d) => {
-//       try {
-//         console.log(d);
-
-//         // res.render('district',
-//         //   {
-
-//         // });
-//       } catch (e) {
-//         res.redirect("/");
-//         console.log("not valid data");
-//       }
-//     });
-// });
-
-// app.post("/bank", function (req, res) {});
-
-app.get("/bank", function (req, res) {});
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server Listening");
 });
