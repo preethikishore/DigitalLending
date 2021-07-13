02e81264fe08e12f7c7b1a75c46381aed743df31
preethikishore <preethikishre@gmail.com>
2021-07-13 16:04:33 +0530

    bank added

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ app.get("/ifsc", function (_req, res) {
   });
 });
 
+app.get("/bank", function (_req, res) {
+  res.render("bank", {
+    accHolderName: "",
+    referenceId: "",
+    message: "",
+  });
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server Listening");
 });
